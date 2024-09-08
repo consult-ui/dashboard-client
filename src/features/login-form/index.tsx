bb870cdb6d865899ebc9f944c99e177f09179a96
Kirill Dubasov <cergocergo41@gmail.com>
2024-09-08 19:55:00 +0300

    remove sign in type select

--- src/features/login-form/index.tsx
+++ src/features/login-form/index.tsx
@@ -3,8 +3,7 @@ import { FormEvent, useState } from 'react';
 
 const LoginForm = () => {
   const [isLoading, setIsLoading] = useState(false);
-  const [isPhone, setIsPhone] = useState(true);
-  const [value, setValue] = useState('');
+  const [login, setLogin] = useState('');
   const [password, setPassword] = useState('');
 
   const onSubmit = (e: FormEvent) => {
@@ -17,40 +16,28 @@ const LoginForm = () => {
     <form onSubmit={onSubmit} className={styles.form} id="Авторизация">
       <h4>Авторизация</h4>
       <div className={styles.inputWrapper}>
-        <label htmlFor="login">{isPhone ? 'Номер телефона' : 'Логин'}</label>
-        <div>
-          <input
-            required
-            value={value}
-            onChange={(e) => setValue(e.target.value)}
-            id="login"
-            type={isPhone ? 'tel' : 'text'}
-            placeholder={isPhone ? 'Введите телефон' : 'Введите логин'}
-          />
-          <div className={styles.btnSwitcher}>
-            <button type="button" disabled={isPhone} onClick={() => setIsPhone(true)}>
-              Телефон
-            </button>
-            <button type="button" disabled={!isPhone} onClick={() => setIsPhone(false)}>
-              Логин
-            </button>
-          </div>
-        </div>
+        <label htmlFor="login">Номер телефона или логин</label>
+        <input
+          required
+          value={login}
+          onChange={(e) => setLogin(e.target.value)}
+          id="login"
+          type="text"
+          placeholder="Введите телефон или логин"
+        />
       </div>
 
       <div className={styles.inputWrapper}>
         <label htmlFor="password">Пароль</label>
-        <div>
-          <input
-            autoComplete="on"
-            required
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            id="password"
-            type="password"
-            placeholder="Введите пароль"
-          />
-        </div>
+        <input
+          autoComplete="on"
+          required
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          id="password"
+          type="password"
+          placeholder="Введите пароль"
+        />
       </div>
 
       <button disabled={isLoading} type={'submit'}>
