49fdfe5a779e75b0aec58ce81a3c2a6d742c7f6a
Kirill Dubasov <cergocergo41@gmail.com>
2024-09-13 14:43:17 +0300

    merge fixes

--- src/features/company-initial-form/index.tsx
+++ src/features/company-initial-form/index.tsx
@@ -1,35 +1,14 @@
 import styles from './CompanyInitialForm.module.css';
-import { EFormFields, IFormData } from './types';
 import Button from '@/shared/ui/button';
 import { Input } from '@/shared/ui/input';
 import { ModalGeneral } from '@/shared/ui/modal-general';
 import { useEffect, useState } from 'react';
 
-const initialData: IFormData = {
-  [EFormFields.INN]: '',
-  [EFormFields.COMPANY_NAME]: '',
-  [EFormFields.DESCRIPTION]: '',
-  [EFormFields.INDUSTRY]: '',
-};
-
 export const CompanyInitialForm = () => {
   const [visibleModal, setVisibleModal] = useState<boolean>(false);
-  const [data, setData] = useState<IFormData>(initialData);
   const [researchInn, setResearchInn] = useState<boolean>(false);
 
-  useEffect(() => {
-    const userData = false;
-    if (!userData) {
-      setVisibleModal(true);
-    }
-  }, []);
-
   const toggleResearch = () => setResearchInn(!researchInn);
-  // const toggleModal = () => setVisibleModal(!visibleModal);
-
-  const handleChangeData = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setData({ ...data, [e.target.name]: e.target.value });
-  };
 
   const submitForm = (e: React.FormEvent) => {
     e.preventDefault();
@@ -40,26 +19,22 @@ export const CompanyInitialForm = () => {
     toggleResearch();
   };
 
+  useEffect(() => {
+    setVisibleModal(true);
+  }, []);
+
   return (
     <ModalGeneral
-      visibleCloseButton={false}
+      // TODO: set to false in future
+      visibleCloseButton={true}
       zIndex={10}
-      styles={{ width: '764px' }}
       title={'Заполните информацию о компании'}
-      onClose={() => {}}
+      onClose={() => setVisibleModal(false)}
       open={visibleModal}
     >
       <form onSubmit={submitForm} className={styles.form}>
         <div className={styles.block}>
-          <Input
-            label={'ИНН'}
-            disabled={researchInn}
-            type="text"
-            id={EFormFields.INN}
-            name={EFormFields.INN}
-            value={data[EFormFields.INN]}
-            onChange={handleChangeData}
-          />
+          <Input label={'ИНН'} disabled={researchInn} type="text" />
           <div className={styles.actions}>
             <Button onClick={onGetInfoInn} size={'md'} disabled={researchInn} variant={'contained'} color={'primary'}>
               Проверить ИНН
@@ -72,40 +47,13 @@ export const CompanyInitialForm = () => {
         </div>
         {researchInn && (
           <>
-            <Input
-              label={'Наименование компании'}
-              type="text"
-              id={EFormFields.COMPANY_NAME}
-              name={EFormFields.COMPANY_NAME}
-              value={data[EFormFields.COMPANY_NAME]}
-              onChange={handleChangeData}
-            />
+            <Input label={'Наименование компании'} type="text" />
             <div className={styles.block}>
-              <label htmlFor={EFormFields.DESCRIPTION}>Описание</label>
-              <textarea
-                rows={5}
-                id={EFormFields.DESCRIPTION}
-                name={EFormFields.DESCRIPTION}
-                value={data[EFormFields.DESCRIPTION]}
-                onChange={handleChangeData as () => void}
-              />
+              <label>Описание</label>
+              <textarea rows={5} />
             </div>
-            <Input
-              label={'Описание'}
-              type="text"
-              id={EFormFields.DESCRIPTION}
-              name={EFormFields.DESCRIPTION}
-              value={data[EFormFields.DESCRIPTION]}
-              onChange={handleChangeData}
-            />
-            <Input
-              label={'Деятельность'}
-              type="text"
-              id={EFormFields.INDUSTRY}
-              name={EFormFields.INDUSTRY}
-              value={data[EFormFields.INDUSTRY]}
-              onChange={handleChangeData}
-            />
+            <Input label={'Описание'} type="text" />
+            <Input label={'Деятельность'} type="text" />
             <div className={styles.actions}>
               <Button size={'md'} variant={'contained'}>
                 Сохранить информацию
